a552185efc26b32e37677982501a9aa912bcfb44
aylan-t <aylan.tighilet@gmail.com>
2024-02-22 13:36:19 -0500

    modif struct.hpp

--- tp3/structures.hpp
+++ tp3/structures.hpp
@@ -1,47 +1,52 @@
 #pragma once
-// Structures mémoires pour une collection de films.
-
 #include <string>
-#include <cassert>
+#include <memory>
 #include "gsl/span"
+using namespace std;
 using gsl::span;
 
-struct Film; struct Acteur; // Permet d'utiliser les types alors qu'ils seront définis après.
+struct Film;
+struct Acteur;
 
 class ListeFilms {
 public:
-	ListeFilms() = default;
-	ListeFilms(const std::string& nomFichier);
-	ListeFilms(const ListeFilms& l) { assert(l.elements == nullptr); } // Pas demandé dans l'énoncé, mais on veut s'assurer qu'on ne fait jamais de copie de liste, car la copie par défaut ne fait pas ce qu'on veut.  Donc on ne permet pas de copier une liste non vide (la copie de liste vide est utilisée dans la création d'un acteur).
-	~ListeFilms();
-	void ajouterFilm(Film* film);
-	void enleverFilm(const Film* film);
-	Acteur* trouverActeur(const std::string& nomActeur) const;
-	span<Film*> enSpan() const;
-	int size() const { return nElements; }
+    ListeFilms() = default;
+    ListeFilms(const string& nomFichier);
+    ListeFilms(const ListeFilms& l) { assert(l.elements == nullptr); }
+    ~ListeFilms();
+    void ajouterFilm(Film* film);
+    void enleverFilm(const Film* film);
+    Acteur* trouverActeur(const string& nomActeur) const;
+    span<Film*> enSpan() const;
+    int size() const { return nElements; }
 
 private:
-	void changeDimension(int nouvelleCapacite);
+    void changeDimension(int nouvelleCapacite);
 
-	int capacite = 0, nElements = 0;
-	Film** elements = nullptr; // Pointeur vers un tableau de Film*, chaque Film* pointant vers un Film.
-	bool possedeLesFilms_ = false; // Les films seront détruits avec la liste si elle les possède.
+    int capacite = 0, nElements = 0;
+    Film** elements = nullptr;
+    bool possedeLesFilms_ = false;
 };
 
 struct ListeActeurs {
-	int capacite, nElements;
-	Acteur** elements; // Pointeur vers un tableau de Acteur*, chaque Acteur* pointant vers un Acteur.
+    int capacite = 0, nElements = 0;
+    unique_ptr<Acteur* []> elements;
+
+    ListeActeurs(int capacite_) : capacite(capacite_), nElements(0), elements(make_unique<Acteur* []>(capacite_)) {}
 };
 
-struct Film
-{
-	std::string titre, realisateur; // Titre et nom du réalisateur (on suppose qu'il n'y a qu'un réalisateur).
-	int anneeSortie, recette; // Année de sortie et recette globale du film en millions de dollars
-	ListeActeurs acteurs;
+struct Film {
+    string titre = "";
+    string realisateur = "";
+    int anneeSortie = 0;
+    int recette = 0;
+    ListeActeurs acteurs;
 };
 
-struct Acteur
-{
-	std::string nom; int anneeNaissance; char sexe;
-	ListeFilms joueDans;
+struct Acteur {
+    string nom = "";
+    int anneeNaissance = 0;
+    char sexe = 'U';
+    ListeFilms joueDans;
 };
+
